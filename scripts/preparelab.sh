65e2dbe5ff47557537841202a47cb77cf60dfa6d
U-VERWALTUNG\krueger <krueger@sabaki.science-computing.de>
2026-07-30 14:32:20 +0200

    fix

--- scripts/preparelab.sh
+++ scripts/preparelab.sh
@@ -12,6 +12,6 @@ sed -i -e "s/$1/$2/g" $2/ansible.cfg
 if [ x$2 = "xlab2" ] ;
 then
    echo "initialising inventory"
-   myuser=§(hostname -f|awk -F. '{print $1}' | awk -F- '{print $2}')
+   myuser=$(cat /etc/myhostname|awk -F. '{print $1}' | awk -F- '{print $2}')
    sed -e s/%%USER%%/${myuser}/g scripts/inventory.yml >${2}/inventory/inventory.yml
 fi
